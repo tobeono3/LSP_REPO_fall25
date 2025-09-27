da1acd196508deae5e470a261cf041d4ba5d8b0e
tobeo <tobeonyekwelu@gmail.com>
2025-09-26 22:58:43 -0400

    commit

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Name: You Know What to Do
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
